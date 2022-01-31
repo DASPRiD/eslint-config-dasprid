56c28fae5b274febd650fda64907c35a9fcdb0d3
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-01-31 13:44:11 +0100

    Add typescript import resolver

--- react.js
+++ react.js
@@ -6,6 +6,9 @@ module.exports = {
         react: {
             version: 'detect',
         },
+        'import/resolver': {
+            typescript: {},
+        },
     },
     parserOptions: {
         ecmaFeatures: {
